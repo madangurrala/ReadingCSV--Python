ced4807a73db604b7379e5c3b5ab8eafa4b5072d
Madan <madangurrala@gmail.com>
2020-04-15 14:53:44 -0400

    Data filtered

--- problem2.py
+++ problem2.py
@@ -1,7 +1,7 @@
 import csv
-import pandas
 
 with open('address - address.csv', mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     for row in csv_reader:
-        print(f'\t{row["First name"]} {row["Last name"]}')
+        if row["Province"] == "Ontario" :
+            print(f'\t{row["First name"]} {row["Last name"]}')
